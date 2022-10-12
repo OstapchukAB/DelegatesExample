27cfde0f7148a24d00e3dfa585143d17008e8626
OstapchukAB <antbg@yandex.ru>
2022-10-12 18:07:41 +0300

    добавил класс AccountEventArgs

---
+++ AccountEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExample
+{
+    class AccountEventArgs
+    {
+
+        // Сообщение
+        public string Message { get; }
+        // Сумма, на которую изменился счет
+        public int Sum { get; }
+        public AccountEventArgs(string message, int sum)
+        {
+            Message = message;
+            Sum = sum;
+        }
+    }
+}
