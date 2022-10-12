1214b94fc9af89000698212054acaca610bc0429
OstapchukAB <antbg@yandex.ru>
2022-10-12 14:05:29 +0300

    in

---
+++ Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesExample
+{
+    public delegate void AccountHandler(string message);
+    class Account
+    {
+        int sum; // Переменная для хранения суммы
+                 
+        // Создаем переменную делегата
+        AccountHandler? taken;
+
+        // через конструктор устанавливается начальная сумма на счете
+        public Account(int sum) => this.sum = sum;
+
+        // Регистрируем делегат
+        public void RegisterHandler(AccountHandler delegat)
+        {
+            taken = delegat;
+        }
+
+        // добавить средства на счет
+        public void Add(int sum) => this.sum += sum;
+        // взять деньги с счета
+        public void Take(int sum)
+        {
+            // берем деньги, если на счете достаточно средств
+            if (this.sum >= sum)
+            {
+                //Console.WriteLine($"Со счета списано {sum} у.е.");
+                this.sum -= sum;
+                // вызываем делегат, передавая ему сообщение
+                taken?.Invoke($"Со счета списано {sum} у.е.");
+            }
+        }
+    }
+}
