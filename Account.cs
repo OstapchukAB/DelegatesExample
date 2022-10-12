bb7bb4b2ef30d86d7566d26bafee92042b062d20
OstapchukAB <antbg@yandex.ru>
2022-10-12 15:07:29 +0300

    Преобразовал бизнес -код аккаунта банковского счета

--- Account.cs
+++ Account.cs
@@ -1,26 +1,38 @@
-﻿namespace DelegatesExample
+﻿using System;
+
+namespace DelegatesExample
 {
     public delegate void AccountHandler(string message);
     class Account
     {
-        int sum; // Переменная для хранения суммы
+        decimal sum; // Переменная для хранения суммы
 
         // Создаем переменную делегата
         public AccountHandler? taken;
 
         // через конструктор устанавливается начальная сумма на счете
-        public Account(int sum) => this.sum = sum;
+        public Account(AccountHandler delegat) 
+        {
+            taken = delegat; 
+            taken?.Invoke($"Счет создан. Баланс:{this.sum:C2}");
+        }
 
         //// Регистрируем делегат
-        //public void RegisterHandler(AccountHandler delegat)
-        //{
-        //    taken = delegat;
-        //}
+        public void RegisterHandler(AccountHandler delegat)
+        {
+            taken = delegat;
+        }
 
         // добавить средства на счет
-        public void Add(int sum) => this.sum += sum;
+        public void Add(decimal sum) 
+        { 
+            this.sum += sum;
+
+            taken?.Invoke($"На счет положена сумма {sum:C2}. Баланс:{this.sum:C2}");
+        }
+        
         // взять деньги с счета
-        public void Take(int sum)
+        public void Take(decimal sum)
         {
             // берем деньги, если на счете достаточно средств
             if (this.sum >= sum)
@@ -28,11 +40,11 @@
                 //Console.WriteLine($"Со счета списано {sum} у.е.");
                 this.sum -= sum;
                 // вызываем делегат, передавая ему сообщение
-                taken?.Invoke($"Со счета списано {sum} у.е.");
+                taken?.Invoke($"Со счета списано {sum:C2}. Баланс:{this.sum:C2}");
             }
             else
             {
-                taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
+                taken?.Invoke($"Недостаточно средств. Невозможно списать:{sum:C2}. Баланс:{this.sum}");
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -4,18 +4,25 @@ class Programm
 {
     public static void Main()
     {
+        System.Globalization.CultureInfo.CurrentCulture = new System.Globalization.CultureInfo("ru-RU");
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
         void PrintSimpleMessage(string message) => Console.WriteLine(message);
 
         // создаем банковский счет
-        Account account = new Account(200);
-        
+        Account account = new Account(PrintSimpleMessage);
+
         // Добавляем в делегат ссылку на метод PrintSimpleMessage
-       // account.RegisterHandler(PrintSimpleMessage);
-        account.taken = PrintSimpleMessage;
+        // account.RegisterHandler(PrintSimpleMessage);
+        //account.taken = PrintSimpleMessage;
+
+        account.Add(200.00M);  
+        
+       
 
         // Два раза подряд пытаемся снять деньги
-        account.Take(100);
-        account.Take(150);
+        account.Take(100.00M);
+        account.Take(150.00M);
         
        Console.WriteLine();
         Console.Write("Для выхода нажмите любую клавишу");
